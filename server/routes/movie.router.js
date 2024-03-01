00e871900c4858402347f5552a18313f7dd2b94f
Beth Tracy <bntracy@gmail.com>
2024-03-01 16:13:02 -0600

    query to database works

--- server/routes/movie.router.js
+++ server/routes/movie.router.js
@@ -20,7 +20,21 @@ router.get('/', (req, res) => {
 
 router.get('/:id', (req, res) => {
   console.log('req.params.id:', req.params.id);
-  res.send(200);
+  const query = `
+    SELECT title, poster, description, name FROM "movies"
+    JOIN "movies_genres" ON movies.id=movies_genres.movie_id
+    JOIN "genres" ON movies_genres.genre_id=genres.id
+    WHERE movies.id=$1;
+  `;
+  pool.query(query, [req.params.id])
+    .then(result => {
+      console.log(result.rows);
+      res.send(result.rows);
+    })
+    .catch(err => {
+      console.log('Error getting movie details', query, err);
+      res.sendStatus(500);
+  });
 });
 
 router.post('/', (req, res) => {
